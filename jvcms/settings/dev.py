22c96f3128b4a7ce8bcfdbaef660f7a42ca03c52
Jonathan E. Villanueva Villanueva <frik_ej2v@hotmail.com>
2022-02-07 18:34:00 -0400

    init

---
+++ jvcms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*sl%whcx#i*k+gqy+(^2c(b57v22##+ks4*$zyilfxpz^gnny('
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
